d28d0ee5f57b7543fd0909e1ec469cbaf952235b
BT_1N3_33 <achen7295@bths.edu>
2018-11-20 11:39:36 -0500

    Merge

--- src/com/company/InPlaceSorts.java
+++ src/com/company/InPlaceSorts.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class InPlaceSorts {
+public class  InPlaceSorts {
 
 
     public InPlaceSorts ()
@@ -94,6 +94,59 @@ public class InPlaceSorts {
         arr[j] = temp;
     }
 
+    public static void mergeSort(int[] elements)
+    {
+        int n = elements.length;
+        int[] temp = new int[n];
+        mergeSortHelper(elements, 0, n-1, temp);
+    }
 
+    private static void mergeSortHelper(int[] elements,
+                                        int from, int to, int[] temp)
+    {
+        if (from < to)
+        {
+            int middle = (from + to) / 2;
+            mergeSortHelper(elements, from, middle, temp);
+            mergeSortHelper(elements, middle + 1, to, temp);
+            merge(elements, from, middle, to, temp);
+        }
+    }
+
+    private static void merge(int[] elements, int from, int mid, int to, int[] temp)
+    {
+        int i = from;
+        int j = mid + 1;
+        int k = from;
+        while (i <= mid && j <= to)
+        {
+            if (elements[i] < elements[j])
+            {
+                temp[k] = elements[i];
+                i++;
+            }
+            else
+            {
+                temp[k] = elements[j];
+                j++;
+            }
+            k++;
+        }
+        while (i <= mid)
+        {
+            temp[k] = elements[i];
+            i++;
+            k++;
+        }
+        while (j <= to)
+        {
+            temp[k] = elements[j];
+            j++;
+            k++;
+        }
+        for (k = from; k <= to; k++) {
+            elements[k] = temp[k];
+        }
+    }
 
 }
